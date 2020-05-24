b8474b655912fe915cf5dc5418f5212bcf0048ea
Виталий <spidometrus@outlook.com>
2020-05-24 20:07:56 +0800

    сделано подключение

---
+++ AuthApp/Model/Context.cs
@@ -0,0 +1,26 @@
+﻿namespace AuthApp.Model
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Context : DbContext
+    {
+        // Контекст настроен для использования строки подключения "Context" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "AuthApp.Model.Context" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "Context" 
+        // в файле конфигурации приложения.
+        public Context()
+            : base("name=Context")
+        {
+            Database.SetInitializer(new Initializer());
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<User> Users { get; set; }
+    }    
+}
---
+++ AuthApp/Model/initializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace AuthApp.Model
+{
+    class Initializer:DropCreateDatabaseIfModelChanges<Context>
+    {
+        protected override void Seed(Context context)
+        {
+            context.Users.Add(new User { Login = "admin", Password = "pikachu" });
+            context.SaveChanges();
+        }
+    }
+}
